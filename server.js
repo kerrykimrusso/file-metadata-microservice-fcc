329ef2563807a1a61796d50d9f1338e5bb6ccc33
System <none>
2017-08-09 02:39:14 +0000

    Checkpoint

--- server.js
+++ server.js
@@ -3,10 +3,8 @@
 
 // init project
 var express = require('express');
+var multer = require('multer')();
 var app = express();
-
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -16,22 +14,12 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
-
 // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
+app.post("/upload", multer.single('file'), function (request, response) {
+  response.json({
+    size: request.file.size
+  });
 });
-
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
